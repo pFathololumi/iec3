35309350a4d0cf37fd87a6e78e3796cd4abfbceb
parisa <parisa@parisa-PC>
2016-02-16 23:57:52 +0330

    add + deposit + withdraw commands

--- src/Server.java
+++ src/Server.java
@@ -1,18 +1,29 @@
 import ServerHandler.FileHandler;
+
 import com.sun.net.httpserver.HttpServer;
+
+import Client.Customer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
 
 public class Server {
 
+	public static HashMap<Integer, Customer> customers;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		customers = new HashMap<Integer, Customer>();
+		
 		HttpServer server = null;
 		try {
-			server = HttpServer.create(new InetSocketAddress(9090), 0);
-			server.createContext("/docs", new FileHandler());
-
+			server = HttpServer.create(new InetSocketAddress(9092), 0);
+			//server.createContext("/docs", new FileHandler());
+			server.createContext("/customer/add", new AddCustomer());
+			server.createContext("/customer/deposit", new DepositHandler());
+			server.createContext("/customer/withdraw", new WithdrawHandler());
 			server.start();
 		} catch (IOException e) {
 			System.out.println("Error Configuring Server");
@@ -20,3 +31,5 @@ public class Server {
 	}
 
 }
+
+
