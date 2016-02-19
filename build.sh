0058cd1ee7fdfe923d626775b7307819be52ecbb
hamed911 <hamed_gorjiara@yahoo.com>
2016-02-19 23:41:27 +0330

    build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf ./bin/*
 find -name "*.java" > sources.txt
-javac -sourcepath src -d bin -classpath ./lib/coolserver.jar @sources.txt?
+javac -sourcepath src -d bin -classpath ./lib/coolserver.jar @sources.txt
 echo Main-Class: server/Server > manifest.txt
 echo Class-Path: lib/coolserver.jar >> manifest.txt
 cd bin
-jar cvfm ../CA2.jar manifest.txt? *
+jar cvfm ../CA2.jar ../manifest.txt *
