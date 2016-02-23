5e653d943edc34481d3f123c6f9bd49164bac9ef
parisa <parisa@parisa-PC>
2016-02-23 23:23:55 +0330

    check GTC error

--- src/domain/dealing/types/GTC.java
+++ src/domain/dealing/types/GTC.java
@@ -1,25 +1,33 @@
 package domain.dealing.types;
 
 import java.io.PrintWriter;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.List;import com.sun.xml.internal.ws.handler.ServerLogicalHandlerTube;
 
 import domain.dealing.BuyingOffer;
 import domain.dealing.Instrument;
+import domain.dealing.Offering;
 import domain.dealing.SellingOffer;
 
 public class GTC implements TypeExecutor{
 
 	@Override
-	public void sellingExecute(PrintWriter out, SellingOffer offer, List<SellingOffer> sellingOffers, List<BuyingOffer> buyingOffers) {
+	public List<List<Offering>> sellingExecute(PrintWriter out, SellingOffer offer, List<SellingOffer> sellingOffers, List<BuyingOffer> buyingOffers) {
 		// TODO Auto-generated method stub
+		List<List<Offering>> offerings = new ArrayList<>();
+		
 		sellingOffers.add(offer);
     	Instrument.sortSellingOfferListByPrice();
+    	offerings.add(sellingOffers);
+    	offerings.add(buyingOffers);
     	
     	if(buyingOffers.isEmpty()){
     		out.println("Order is queued");
-    		return;
+    		return offering;
     	}
     	Instrument.matchingOffers(out,true);
+    	
+    	
 	}
 
 	@Override
